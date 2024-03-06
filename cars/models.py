ff3d2f932767e9b243f50e911c3da09311f6654d
Ali-Sina-255 <alisinasultani255@gmail.com>
2024-03-06 13:26:22 +0430

    car model is created

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,117 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Car(models.Model):
+    STATE_CHOICES = (
+        ('AL', 'Alabama'),
+        ('AK', 'Alaska'),
+        ('AZ', 'Arizona'),
+        ('AR', 'Arkansas'),
+        ('CA', 'California'),
+        ('CO', 'Colorado'),
+        ('CT', 'Connecticut'),
+        ('DE', 'Delaware'),
+        ('DC', 'District Of Columbia'),
+        ('FL', 'Florida'),
+        ('GA', 'Georgia'),
+        ('HI', 'Hawaii'),
+        ('ID', 'Idaho'),
+        ('IL', 'Illinois'),
+        ('IN', 'Indiana'),
+        ('IA', 'Iowa'),
+        ('KS', 'Kansas'),
+        ('KY', 'Kentucky'),
+        ('LA', 'Louisiana'),
+        ('ME', 'Maine'),
+        ('MD', 'Maryland'),
+        ('MA', 'Massachusetts'),
+        ('MI', 'Michigan'),
+        ('MN', 'Minnesota'),
+        ('MS', 'Mississippi'),
+        ('MO', 'Missouri'),
+        ('MT', 'Montana'),
+        ('NE', 'Nebraska'),
+        ('NV', 'Nevada'),
+        ('NH', 'New Hampshire'),
+        ('NJ', 'New Jersey'),
+        ('NM', 'New Mexico'),
+        ('NY', 'New York'),
+        ('NC', 'North Carolina'),
+        ('ND', 'North Dakota'),
+        ('OH', 'Ohio'),
+        ('OK', 'Oklahoma'),
+        ('OR', 'Oregon'),
+        ('PA', 'Pennsylvania'),
+        ('RI', 'Rhode Island'),
+        ('SC', 'South Carolina'),
+        ('SD', 'South Dakota'),
+        ('TN', 'Tennessee'),
+        ('TX', 'Texas'),
+        ('UT', 'Utah'),
+        ('VT', 'Vermont'),
+        ('VA', 'Virginia'),
+        ('WA', 'Washington'),
+        ('WV', 'West Virginia'),
+        ('WI', 'Wisconsin'),
+        ('WY', 'Wyoming'),
+    )
+
+    year_choice = []
+    for r in range(2000, (datetime.now().year + 1)):
+        year_choice.append((r, r))
+
+    FEATURES_CHOICES = (
+        ('Cruise Control', 'Cruise Control'),
+        ('Audio Interface', 'Audio Interface'),
+        ('Airbags', 'Airbags'),
+        ('Air Conditioning', 'Air Conditioning'),
+        ('Seat Heating', 'Seat Heating'),
+        ('Alarm System', 'Alarm System'),
+        ('ParkAssist', 'ParkAssist'),
+        ('Power Steering', 'Power Steering'),
+        ('Reversing Camera', 'Reversing Camera'),
+        ('Direct Fuel Injection', 'Direct Fuel Injection'),
+        ('Auto Start/Stop', 'Auto Start/Stop'),
+        ('Wind Deflector', 'Wind Deflector'),
+        ('Bluetooth Handset', 'Bluetooth Handset'),
+    )
+    DOOR_CHOICES = (
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
+        ('6', '6'),
+    )
+    cart_title = models.CharField(max_length=255)
+    state = models.CharField(max_length=255, choices=STATE_CHOICES)
+    city = models.CharField(max_length=200)
+    color = models.CharField(max_length=200)
+    model = models.CharField(max_length=200)
+    year = models.IntegerField(('year',), choices=year_choice)
+    condition = models.CharField(max_length=200)
+    description = models.TextField(max_length=500)
+    price = models.IntegerField()
+    car_photo = models.ImageField(upload_to='photos/%Y/%/%m/%d/', blank=True)
+    car_photo_1 = models.ImageField(upload_to='photos/%Y/%/%m/%d/', blank=True)
+    car_photo_2 = models.ImageField(upload_to='photos/%Y/%/%m/%d/', blank=True)
+    car_photo_3 = models.ImageField(upload_to='photos/%Y/%/%m/%d/', blank=True)
+    car_photo_4 = models.ImageField(upload_to='photos/%Y/%/%m/%d/', blank=True)
+    features = models.CharField(max_length=100,choices=FEATURES_CHOICES)
+    body_style = models.CharField(max_length=100)
+    engine = models.CharField(max_length=100)
+    transmission = models.CharField(max_length=100)
+    interior = models.CharField(max_length=100)
+    mils = models.IntegerField()
+    doors = models.CharField(max_length=100, choices=DOOR_CHOICES)
+    passengers = models.CharField(max_length=100)
+    vin_no = models.CharField(max_length=100)
+    milage = models.CharField(max_length=100)
+    fuel_type = models.CharField(max_length=10)
+    no_of_owners = models.CharField(max_length=100)
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.cart_title
+    
--- core/urls.py
+++ core/urls.py
@@ -19,10 +19,9 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('car/', include('cars.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
