985d945bf37e82071e8bfeaca81c31bb7eca8790
Ali-Sina-255 <alisinasultani255@gmail.com>
2024-03-06 22:05:15 +0430

    car model is updated

--- cars/admin.py
+++ cars/admin.py
@@ -6,7 +6,8 @@ from django.utils.html import format_html
 class CarAdmin(admin.ModelAdmin):
 	def thumbinail(self, object):
 		return format_html('<img src="{}" width="40" style="border-radius:50px" />'.format(object.car_photo.url))
-	list_display = ('id','cart_title','city','color','model','year','body_style','fuel_type','is_featured')
+	
+	list_display = ('id','thumbinail','cart_title','city','color','model','year','body_style','fuel_type','is_featured')
 	list_display_links = ('id','cart_title')
 	list_editable = ('is_featured',)
 	search_fields = ('cart_title','city','body_style','model','fuel_type')
